3a3694ec98ab0f8d157fb271ccf9f7e2cdab631d
lektoakle <oknezrob@gmail.com>
2026-04-20 20:33:32 +0300

    added a test opening webpage

---
+++ QA33_51_Start_Selenium/src/test/java/Start_Selenium.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+import org.testng.annotations.Test;
+
+public class Start_Selenium {
+
+    WebDriver wd;
+
+    @Test
+    public void test() {
+        wd = new ChromeDriver();
+//        wd.get("https://www.google.com/");
+        wd.navigate().to("https://www.google.com/");
+        wd.navigate().back();
+        wd.navigate().forward();
+        wd.navigate().refresh();
+        wd.quit();
+        
+    }
+}
